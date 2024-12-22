5e07f9a4b78d3558e6b9f0904c50fc8543839aa6
Per Kulseth Dahl <pkdahl@uio.no>
2024-12-22 19:14:28 +0100

    Event with venue

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,17 @@
 use std::io::Write;
 use icalendar::{Calendar, CalendarDateTime, Class, Component, Event, EventLike, Property, Todo};
+use icalendar::{EventStatus, Venue};
 use chrono::{Duration, NaiveDate, NaiveTime, Utc};
 
 fn main() {
+    let venue = Venue::new()
+        .street_address("Kniksens plass 1")
+        .postal_code("5063")
+        // .extended_address("5063 Bergen")
+        .locality("Bergen")
+        .region("Vestland")
+        .country("Norge")
+        .done();
     let my_calendar = Calendar::new()
         .name("example calendar")
         .push(
@@ -22,35 +31,18 @@ fn main() {
                 .done(),
         )
         .push(
-            // add a todo
-            Todo::new()
-                .summary("groceries")
-                .description("Buy some milk")
-                .done(),
-        )
-        .push(
-            // add an all-day event
             Event::new()
-                .all_day(NaiveDate::from_ymd_opt(2016, 3, 15).unwrap())
-                .summary("My Birthday")
-                .description("Hey, I'm gonna have a party\nBYOB: Bring your own beer.\nHendrik")
-                .done(),
-        )
-        .push(
-            // event with utc timezone
-            Event::new()
-                .starts(CalendarDateTime::from(
-                    NaiveDate::from_ymd_opt(2024, 10, 24).unwrap()
-                        .and_time(NaiveTime::from_hms_opt(20, 10, 00).unwrap())
-                        .and_utc()
-                ))
-                .summary("Birthday Party")
-                .description("I'm gonna have a party\nBYOB: Bring your own beer.\nHendrik")
-                .done(),
+            .summary("Brann—PAOK")
+            .starts(Utc::now() + Duration::days(1))
+            .ends(Utc::now() + Duration::days(1) + Duration::hours(2))
+            .status(EventStatus::Confirmed)
+            .append_component(venue)
+            .done()
         )
         .done();
 
-    println!("{}", my_calendar);
+    // println!("{}", my_calendar);
+    // println!("{}", venue.to_string());
     let mut file = std::fs::File::create("calendar.ics").unwrap();
     writeln!(file, "{}", my_calendar).unwrap();
 }
